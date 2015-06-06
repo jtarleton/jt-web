4ac17e54f23b0b923647bccf5640d51c042ab67a
James Tarleton <jamestarleton@gmail.com>
2015-06-06 11:33:46 -0400

    index

--- web/index.php
+++ web/index.php
@@ -5,4 +5,12 @@ date_default_timezone_set('America/New_York');
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('front', 'prod', false);
+
+
+
+\Facebook\FacebookSession::setDefaultApplication('129560680445301', 'fe6c2dc777675c1091e2e8b59b5b31fc');
+
+
+
+
 sfContext::createInstance($configuration)->dispatch();
