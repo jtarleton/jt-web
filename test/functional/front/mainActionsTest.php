7904895141485c123d8851c4998fdb1f08bf9fe0
jtarleton <jamestarleton@gmail.com>
2016-08-31 16:56:54 -0400

    updates

--- test/functional/front/mainActionsTest.php
+++ test/functional/front/mainActionsTest.php
@@ -1,19 +1,13 @@
 <?php
-
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
-
 $browser = new sfTestFunctional(new sfBrowser());
-
 $browser->
   get('/index')->
-
   with('request')->begin()->
     isParameter('module', 'main')->
     isParameter('action', 'index')->
-  end()->
-
-  with('response')->begin()->
+  end();
+  //with('response')->begin()->
     //isStatusCode(302)->
     //checkElement('body', '/Homepage/')->
-  end()
-;
+  //end()
